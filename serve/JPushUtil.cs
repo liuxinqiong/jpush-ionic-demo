5478112409e59512cf1b353b8ec6a97122af8b97
liuxinqiong <372513646@qq.com>
2017-03-28 20:05:35 +0800

    first commit

---
+++ serve/JPushUtil.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cn.jpush.api;
+using Newtonsoft.Json;
+using cn.jpush.api.push.mode;
+using cn.jpush.api.push.notification;
+using cn.jpush.api.push;
+
+namespace JPushTest
+{
+    class JPushUtil
+    {
+        private static string appKey = "a650733b66ef847d45d63959"; // Your App Key from JPush
+        private static string masterSecret = "cc041c2cbe82d9f54741c23c"; // Your Master Secret from JPush
+
+        /*
+         * @return MessageResult
+         * msg_id 返回消息号
+         * sendno 由你赋值的字段，这里会传递回来
+         * ResponseResult http返回的相关信息
+         */
+        public MessageResult sendNotification(string content)
+        {
+            JPushClient client = new JPushClient(appKey, masterSecret);
+            PushPayload pushPayload = new PushPayload();
+
+            //step1:Platform 平台信息（必填），这里不纠结，直接选择全平台
+            pushPayload.platform = Platform.all();
+
+            //step2：准备告警内容
+            pushPayload.notification = createNotification(content);
+
+            //step3: option设置
+            pushPayload.options = initOption();
+
+            //step4：发送目标,JPush提供了多种方式，别名，标签，注册id，分群，广播等。s_表示是静态函数的意思，Audience中还有个tag的成员函数，为了避免命名冲突
+            pushPayload.audience = Audience.all();//测试是暂时使用all
+
+            //last step:发送并返回结果
+            return client.SendPush(pushPayload);
+        }
+
+        private Notification createNotification(string content)
+        {
+            Notification not = new Notification();
+            //统一制定设置通知内容，如果各平台单独指定，这里会被覆盖
+            not.setAlert(content);
+            not.setAndroid(createAndroidNotification());
+            not.setIos(createIosNotification());
+            return not;
+        }
+
+        private IosNotification createIosNotification()
+        {
+            //定制ios效果
+            IosNotification ios = new IosNotification();
+            ios.setSound("default");//必须设置，否则无声音
+            ios.setContentAvailable(false);//非静默推送
+            //ios.setCategory();ios8才支持
+            ios.autoBadge();//原有的badge基础上+1
+            //ios.incrBadge(5);原有的badge值加上你所设定值
+            return ios;
+        }
+
+        private AndroidNotification createAndroidNotification()
+        {   
+            //定制android效果
+            AndroidNotification android = new AndroidNotification();
+            //android.setTitle();如果指定了，则通知里原来展示App名称的地方，将展示成这个字段。
+            //android.setBuilderID();Android SDK 可设置通知栏样式，这里根据样式 ID 来指定该使用哪套样式。
+            android.setPriority(2);//表示最高优先级PRIORIY_MAX
+            android.setCategory("alarm");
+            //设置 提示音，振动，指示灯 sdk暂时不支持
+            //android.alert_type = 1 | 2 | 4;
+            return android;
+        }
+
+        private Options initOption()
+        {
+            Options ops = new Options();
+            ops.time_to_live = 86400 * 10;//离线消息保存10天,已经是最大值
+            //ops.override_msg_id //设置是否覆盖上一条通知
+            //ops.apns_production //True 表示推送生产环境，False 表示要推送开发环境； 如果不指定则为推送生产环境。
+            //ops.big_push_duration 缓慢推送，把原本尽可能快的推送速度，降低下来，在给定的 n 分钟内，均匀地向这次推送的目标用户推送最大值为 1440。未设置则不是定速推送。
+            //ops.sendno 纯粹用来作为 API 调用标识，API 返回时被原样返回，以方便 API 调用方匹配请求与返回。
+            return ops;
+        }
+
+        /*
+         * 更多高级功能：
+         * 1.Message 消息内容（可选）应用内消息。
+         *      自定义消息，透传消息。此部分内容不会展示到通知栏上，JPush SDK 收到消息内容后透传给 App。App 需要自行处理。
+         * 2.Received API 以 msg_id 作为参数，去获取该 msg_id 的送达统计数据。
+         *      由于统计数据并非非是即时的,所以等待一小段时间再执行下面的获取结果方法
+         *      System.Threading.Thread.Sleep(10000);
+         *      ReceivedResult getReceivedApi(String msg_ids)
+         * 3.提供一定时间段的用户相关统计数据：新增用户、在线用户、活跃用户。vip用户专用
+         *      UsersResult getReportUsers(TimeUnit timeUnit, String start, int duration)
+         * 4.消息统计查询接口，这个接口是vip用户专用
+         *      MessagesResult getReportMessages(params String[] msgIds)
+         * 5.Device-API,选择客户端设置，这里了解即可
+         *      Device API 用于在服务器端查询、设置、更新、删除设备的 tag,alias 信息，使用时需要注意不要让服务端设置的标签又被客户端给覆盖了。
+         *      如果不是熟悉 tag，alias 的逻辑建议只使用客户端或者服务端二者中的一种。如果是两边同时使用，请确认自己应用可以处理好标签和别名的同步。
+         *      主要api有
+         *      Method TagAliasResult getDeviceTagAlias(String registrationId)
+         *      Method - DefaultResult updateDeviceTagAlias(String registrationId, bool clearAlias, bool clearTag)
+         *      Method - DefaultResult updateDeviceTagAlias(String registrationId,String alias,HashSet tagsToAdd,HashSet tagsToRemove)
+         *      Method - TagListResult getTagList()获取当前应用的所有标签列表
+         *      Method - BooleanResult isDeviceInTag(String theTag, String registrationID)
+         *      Method - public DefaultResult addRemoveDevicesFromTag(String theTag,HashSet toAddUsers, HashSet toRemoveUsers)
+         *      Method - public DefaultResult deleteTag(String theTag, String platform)
+         *      Method - AliasDeviceListResult getAliasDeviceList(String alias, String platform)
+         *      Method - public DefaultResult deleteAlias(String alias, String platform)
+         */
+    }
+}
---
+++ www/js/services.js
@@ -0,0 +1,163 @@
+/**
+ * Created by Administrator on 2017/3/23.
+ */
+jpushdemo.factory('jpushService', ['$http', '$window', '$document', function ($http, $window, $document) {
+
+  //启动极光推送
+  var _init = function (config) {
+    if (ionic.Platform.isIOS()) {
+      //ios不同于android，需要注册apns服务
+      $window.plugins.jPushPlugin.startJPushSDK();
+    }
+    //本功能是一个完全本地的状态操作，也就是说：停止推送服务的状态不会保存到服务器上。
+    //推送服务停止期间推送的消息，恢复推送服务后，如果推送的消息还在保留的时长范围内，则客户端是会收到离线消息。
+    $window.plugins.jPushPlugin.init();
+    //设置tag和Alias触发事件处理
+    document.addEventListener('jpush.setTagsWithAlias', config.stac, false);
+    //打开推送消息事件处理
+    $window.plugins.jPushPlugin.openNotificationInAndroidCallback = config.oniac;
+    //开发周期建议打开debug模式，可以获得额外帮助
+    $window.plugins.jPushPlugin.setDebugMode(true);
+    //一下仅限android，插件内部做了平台判断
+    $window.plugins.jPushPlugin.setBasicPushNotificationBuilder();//声音，振动提示等,
+    $window.plugins.jPushPlugin.requestPermission();//用于在 Android 6.0 及以上系统，申请一些权限
+
+  }
+
+  /**
+   * @function: 停止极光推送
+   * android平台:
+   * JPush Service 不在后台运行。
+   * 收不到推送消息。
+   * 不能通过 JPushInterface.init 恢复，需要调用 resumePush 恢复。
+   * 极光推送所有的其他 API 调用都无效。
+   * ios平台：
+   * 不推荐调用，因为这个 API 只是让你的 DeviceToken 失效，在 设置－通知 中您的应用程序没有任何变化。
+   * 建议设置一个 UI 界面， 提醒用户在 设置－通知 中关闭推送服务。
+   * */
+  var _stopPush = function () {
+    $window.plugins.jPushPlugin.stopPush();
+  }
+
+  /**
+   * @function 重启极光推送
+   * @description Android平台:极光推送完全恢复正常工作。
+   * @description iOS平台:重新去 APNS 注册。
+   * */
+  var _resumePush = function () {
+    $window.plugins.jPushPlugin.resumePush();
+  }
+
+  /**
+   * @function: 获取状态
+   * @description Android 平台:用来检查 Push Service 是否已经被停止。
+   * @description iOS 平台:平台检查推送服务是否注册。
+   * @param 参数是回调函数：result为0表示开启，其他表示关闭
+   * */
+  var _isPushStopped = function (fun) {
+    $window.plugins.jPushPlugin.isPushStopped(fun)
+  }
+
+  /**
+   * @function 得到RegistrationID
+   * @description 集成了 JPush SDK 的应用程序在第一次成功注册到 JPush 服务器时，JPush 服务器会给客户端返回一个唯一的该设备的标识 - RegistrationID。
+   * @description 应用程序可以把此 RegistrationID 保存以自己的应用服务器上，然后就可以根据 RegistrationID 来向设备推送消息或者通知。
+   * @param 参数是回调函数，result结果为RegistrationID
+   * */
+  var _getRegistrationID = function (fun) {
+    window.plugins.jPushPlugin.getRegistrationID(fun);
+  }
+
+  /**
+   * 设置别名和标签，本身无返回值，可以注册jpush.setTagsWithAlias 事件来监听设置结果:
+   * document.addEventListener("jpush.setTagsWithAlias", function(event) {}, false)
+   * event结构为{resultCode:null,tags:null,alias:null}
+   * 具体错误码定义见readme.md
+   * */
+
+  /**
+   * @function 设置别名
+   * @description 为安装了应用程序的用户，取个别名来标识。以后给该用户 Push 消息时，就可以用此别名来指定。尽可能根据别名来唯一确定用户。
+   * @alias 参数类型为字符串。空字符串 （""）表示取消之前的设置。
+   * */
+  var _setAlias = function (alias) {
+    $window.plugins.jPushPlugin.setAlias(alias);
+  }
+
+  /**
+   * @function 设置标签
+   * @description 为安装了应用程序的用户，打上标签。其目的主要是方便开发者根据标签，来批量下发 Push 消息。
+   * @description 可为每个用户打多个标签。
+   * @tags 参数类型为数组。空集合表示取消之前的设置。
+   * @description 每个 tag 命名长度限制为 40 字节，最多支持设置 100 个 tag，但总长度不得超过1K字节（判断长度需采用 UTF-8 编码）。
+   * @description 单个设备最多支持设置 100 个 tag，App 全局 tag 数量无限制。
+   * */
+  var _setTags = function (tags) {
+    $window.plugins.jPushPlugin.setTags(tags);
+  }
+
+  //设置标签和别名
+  var _setTagsWithAlias = function (tags, alias) {
+    $window.plugins.jPushPlugin.setTagsWithAlias(tags, alias);
+  }
+
+  /**
+   * @function 判断系统设置中是否允许当前应用推送
+   * @description 在 Android 中，返回值为 0 时，代表系统设置中关闭了推送；为 1 时，代表打开了推送（目前仅适用于Android 4.4+）。
+   * @description 在 iOS 中，返回值为 0 时，代表系统设置中关闭了推送；大于 0 时，代表打开了推送，且能够根据返回值判断具体通知形式
+   * @param 参数是回调方法，result状态值
+   * */
+  var _getUserNotificationSettings = function (fun) {
+    window.plugins.jPushPlugin.getUserNotificationSettings(fun);
+  }
+
+  var _clearLocalNotifications = function () {
+    window.plugins.jPushPlugin.clearLocalNotifications();
+  }
+
+  // iOS methods
+  var _setBadge=function (value) {
+    window.plugins.jPushPlugin.setBadge(value);
+  }
+
+  var _resetBadge=function () {
+    window.plugins.jPushPlugin._resetBadge();
+  }
+
+  var _addLocalNotificationForIOS=function (delayTime, content, badge, notificationID, extras) {
+    window.plugins.jPushPlugin.addLocalNotificationForIOS(delayTime, content,
+      badge, notificationID, extras);
+  }
+
+
+
+  return {
+    init: _init,
+    isPushStopped: _isPushStopped,
+    stopPush: _stopPush,
+    resumePush: _resumePush,
+    setTagsWithAlias: _setTagsWithAlias,
+    setTags: _setTags,
+    setAlias: _setAlias,
+    getRegistrationID:_getRegistrationID,
+    getUserNotificationSettings:_getUserNotificationSettings
+  };
+}])
+
+
+  .factory('noticeService', [function () {
+    var notices = [
+      {id: 1, msg: '消息一'},
+      {id: 2, msg: '消息二'},
+      {id: 3, msg: '消息三'},
+      {id: 4, msg: '消息四'},
+      {id: 5, msg: '消息五'},
+      {id: 6, msg: '消息六'},
+      {id: 7, msg: '消息七'},
+      {id: 8, msg: '消息八'}
+    ];
+
+    return {
+      notices: notices
+    };
+  }])
